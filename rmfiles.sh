e492042563c592af766a5c8bc427bb61c5f129c9
Roma K <romankuchukov@gmail.com>
2020-08-11 02:40:59 -0400

    removed comments

--- rmfiles.sh
+++ rmfiles.sh
@@ -1,28 +1,15 @@
 start=$(gdate +%s%N)
-removedFiles=()
+files=()
 
-# test directories and sym links
-# echo '--------------------------------------------------'
-# find . -maxdepth 1 -iname '*' | while read file; do
-#     if [[ ! -d $file && -f $file && "$file" != $0 ]]; then
-#         echo "File removed: $file"
-#         rm "$file"
-#     fi
-# done
-# echo 'Finished.'
-# echo '--------------------------------------------------'
-
-#test directories and sym links
 echo '--------------------------------------------------'
 
 for file in *; do [[ ! -d $file && -f $file && "./$file" != $0 ]] || continue
-    #echo "File removed: $file"
-    removedFiles+=("File removed: $file")
+    files+=("File removed: $file")
     rm "$file"
 done
 
 end=$((($(gdate +%s%N) - $start)/1000000))
-printf '%s\n' "${removedFiles[@]}"
+printf '%s\n' "${files[@]}"
 
 echo "Finished in: $end milliseconds"
 echo '--------------------------------------------------'
