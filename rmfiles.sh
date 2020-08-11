6e77c778f4754e9d341834ef4b542e8e9c1700b4
romakuchukov <uwc.mail@gmail.com>
2020-08-11 02:57:39 -0400

    Removed storage array.

--- rmfiles.sh
+++ rmfiles.sh
@@ -1,15 +1,13 @@
 start=$(gdate +%s%N)
-files=()
 
 echo '--------------------------------------------------'
 
 for file in *; do [[ ! -d $file && -f $file && "./$file" != $0 ]] || continue
-    files+=("File removed: $file")
+    "File removed: $file"
     rm "$file"
 done
 
 end=$((($(gdate +%s%N) - $start)/1000000))
-printf '%s\n' "${files[@]}"
 
 echo "Finished in: $end milliseconds"
 echo '--------------------------------------------------'
